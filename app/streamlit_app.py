cbbccb2519f30dbe0278d9c725e99dae95fc74b2
ShamlanAhamed <romanempirewill121@gmail.com>
2023-12-26 22:08:50 +0530

    commit

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,3 +1,31 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:822ee524971fad2b7e1aa2c3f94cca2c751b75c045e12087713c7c5e1ff157c5
-size 841
+import streamlit as st
+from PIL import Image
+from model.retinaModel import load_retina_model
+from utils.image_preprocessing import preprocess_input_image
+
+model_path = "model/ey"
+model = load_retina_model(model_path)
+
+def load_and_predict(upload_file):
+    img_array = preprocess_input_image(upload_file)
+    prediction = model.predict(img_array, batch_size = None, step = 1)
+    return prediction
+
+
+def display_prediction(prediction):
+    # Interpret the prediction
+    if prediction[0][0] > 0.5:
+        
+        st.header('Healthy')
+        st.warning('Good job')
+        st.balloons()
+    else:
+        print("Predicted: Diseased")
+        st.header('Diseased')
+        st.success('please consult doctor')
+
+def main():        
+    st.title('Input | Capture faces to detect mask')    
+    
+if __name__ == "__main__":
+    main()
